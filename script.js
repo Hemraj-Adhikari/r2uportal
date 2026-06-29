d0d681f8e651cb4852514005f54b680da842db91
Hemraj Adhikari <hemrajadhikari2025@gmail.com>
2026-06-29 20:51:02 +0545

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 /* ═══════════ FIREBASE CONFIGURATION ═══════════ */
 const firebaseConfig = {
-  apiKey: "AIzaSyB-xxxxxxxxxxxxxxx", // Aafno actual key halnu
+  apiKey: "AIzaSyC-gxvykJgzcU8MCrvqZO5py2nipSYy4P0",
   authDomain: "portal-8f42b.firebaseapp.com",
   projectId: "portal-8f42b",
-  storageBucket: "portal-8f42b.appspot.com",
-  messagingSenderId: "xxxxxxxxxxx",
-  appId: "1:xxxxxxxxxx:web:xxxxxxxxxx"
+  storageBucket: "portal-8f42b.firebasestorage.app",
+  messagingSenderId: "770003878980",
+  appId: "1:770003878980:web:380022261ef4be8e6a6811"
 };
 
 // Initialize Firebase
