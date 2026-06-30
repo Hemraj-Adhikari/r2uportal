2982fa8e49cfd7234d358477dd6d8bff387e17b3
Hemraj Adhikari <hemrajadhikari2025@gmail.com>
2026-06-30 06:25:43 +0545

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyC-gxvykJgzcU8MCrvqZO5py2nipSYy4P0",
-  authDomain: "portal-8f42b.firebaseapp.com",
-  projectId: "portal-8f42b",
-  storageBucket: "portal-8f42b.firebasestorage.app",
-  messagingSenderId: "770003878980",
-  appId: "1:770003878980:web:380022261ef4be8e6a6811"
-};
